4f2fb67d07771779e7b85c3a2f934e07d8359d4d
Chris James <chris.james@saltpay.co>
2022-06-24 16:49:26 +0100

    refactor command line driver

--- cmd/command-line/test_driver.go
+++ cmd/command-line/test_driver.go
@@ -50,36 +50,14 @@ func NewCommandLineTempConverterDriver() (*CommandLineTempConverterDriver, error
 }
 
 func (c *CommandLineTempConverterDriver) ConvertFromFahrenheitToCelsius(ctx context.Context, fahrenheit float64) (celsius float64, err error) {
-	cmd := exec.Command(c.cmdPath)
-	cmdStdin, err := cmd.StdinPipe()
-	if err != nil {
-		return 0, err
-	}
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
-
-	if err := cmd.Start(); err != nil {
-		return 0, fmt.Errorf("cannot run temp converter: %s", err)
-	}
-	fmt.Fprintln(cmdStdin, "C")
-	fmt.Fprintln(cmdStdin, fmt.Sprintf("%.2f", fahrenheit))
-	cmd.Wait()
-
-	lines := strings.Split(out.String(), "\n")
-	lastLine := lines[len(lines)-2]
-	indexFunc := strings.LastIndexFunc(lastLine, func(r rune) bool {
-		return r == ':'
-	})
-	celsius, err = strconv.ParseFloat(lastLine[indexFunc+2:], 2)
-	if err != nil {
-		return 0.0, err
-	}
-
-	return celsius, err
+	return c.runProgram(ctx, "C", fahrenheit)
 }
 
 func (c *CommandLineTempConverterDriver) ConvertFromCelsiusToFahrenheit(ctx context.Context, celsius float64) (fahrenheit float64, err error) {
+	return c.runProgram(ctx, "F", celsius)
+}
+
+func (c *CommandLineTempConverterDriver) runProgram(ctx context.Context, choice string, temp float64) (conversion float64, err error) {
 	cmd := exec.Command(c.cmdPath)
 	cmdStdin, err := cmd.StdinPipe()
 	if err != nil {
@@ -92,8 +70,8 @@ func (c *CommandLineTempConverterDriver) ConvertFromCelsiusToFahrenheit(ctx cont
 	if err := cmd.Start(); err != nil {
 		return 0, fmt.Errorf("cannot run temp converter: %s", err)
 	}
-	fmt.Fprintln(cmdStdin, "F")
-	fmt.Fprintln(cmdStdin, fmt.Sprintf("%.2f", celsius))
+	fmt.Fprintln(cmdStdin, choice)
+	fmt.Fprintln(cmdStdin, fmt.Sprintf("%.2f", temp))
 	cmd.Wait()
 
 	lines := strings.Split(out.String(), "\n")
@@ -101,13 +79,12 @@ func (c *CommandLineTempConverterDriver) ConvertFromCelsiusToFahrenheit(ctx cont
 	indexFunc := strings.LastIndexFunc(lastLine, func(r rune) bool {
 		return r == ':'
 	})
-	f := lastLine[indexFunc+2:]
-	fahrenheit, err = strconv.ParseFloat(f, 2)
+	result, err := strconv.ParseFloat(lastLine[indexFunc+2:], 2)
 	if err != nil {
 		return 0.0, err
 	}
 
-	return fahrenheit, err
+	return result, err
 }
 
 func (c *CommandLineTempConverterDriver) Cleanup() {
